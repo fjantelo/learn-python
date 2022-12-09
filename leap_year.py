7b54a81bb8ff955b585e0e35afe7b381e4531c39
Fernando Antelo <fjantelo@gmail.com>
2022-12-09 12:25:52 -0500

    Adds leap year

---
+++ leap_year.py
@@ -0,0 +1,26 @@
+# Given a year, report if it is a leap year.
+
+# The tricky thing here is that a leap year in the Gregorian calendar occurs:
+
+# on every year that is evenly divisible by 4
+# except every year that is evenly divisible by 100
+# unless the year is also evenly divisible by 400
+# For example, 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is.
+
+# If your language provides a method in the standard library that does this look-up, pretend it doesn't exist and implement it yourself.
+
+def leap_year(year):
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
+
+
+print(leap_year(1997))
+print(leap_year(1996))
+print(leap_year(1900))
+print(leap_year(2000))
