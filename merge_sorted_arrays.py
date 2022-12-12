0cfab65da2ee2d220e9869cfc752e282d252eb02
Fernando Antelo <fjantelo@gmail.com>
2022-12-12 13:26:50 -0500

    Changes logic in merge sorted arrays to account for additional test cases

--- merge_sorted_arrays.py
+++ merge_sorted_arrays.py
@@ -10,10 +10,16 @@ def merge_sorted_arrays(array1, array2):
     output = []
     index1 = index2 = 0
     while index1 < len(array1) or index2 < len(array2):
-        if index1 == len(array1) or array1[index1] > array2[index2]:
+        if index1 == len(array1):
             output.append(array2[index2])
             index2 += 1
-        elif index2 == len(array2) or array2[index2] > array1[index1]:
+        elif index2 == len(array2):
+            output.append(array1[index1])
+            index1 += 1
+        elif array1[index1] > array2[index2]:
+            output.append(array2[index2])
+            index2 += 1
+        elif array2[index2] > array1[index1]:
             output.append(array1[index1])
             index1 += 1
         elif array1[index1] == array2[index2]:
@@ -25,3 +31,8 @@ def merge_sorted_arrays(array1, array2):
 
 
 print(merge_sorted_arrays([1, 5, 8], [6, 9]))
+print(merge_sorted_arrays([1, 5, 8], [6, 7]))
+print(merge_sorted_arrays([1, 5, 8], [10, 12]))
+print(merge_sorted_arrays([1], [1]))
+print(merge_sorted_arrays([1], [3]))
+print(merge_sorted_arrays([3], [0]))
