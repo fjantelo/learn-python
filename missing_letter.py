eaa827af8c5fb71c39b461ff8777d7678102a199
Fernando Antelo <fjantelo@gmail.com>
2022-12-14 13:54:16 -0500

    Adds missing letter

---
+++ missing_letter.py
@@ -0,0 +1,45 @@
+# A given string contains all the letters from the alphabet except for one. Return the missing letter.
+
+# NOTE: You must accomplish this in O(n) time. This is also known as linear time.
+
+# Input: “The quick brown box jumps over a lazy dog”
+# Output: “f”
+
+def missing_letter(string):
+    l_string = string.casefold()
+    alphabet = {
+        "a": 0,
+        "b": 0,
+        "c": 0,
+        "d": 0,
+        "e": 0,
+        "f": 0,
+        "g": 0,
+        "h": 0,
+        "i": 0,
+        "j": 0,
+        "k": 0,
+        "l": 0,
+        "m": 0,
+        "n": 0,
+        "o": 0,
+        "p": 0,
+        "q": 0,
+        "r": 0,
+        "s": 0,
+        "t": 0,
+        "u": 0,
+        "v": 0,
+        "w": 0,
+        "x": 0,
+        "y": 0,
+        "z": 0,
+    }
+    for letter in l_string:
+        if letter in alphabet.keys():
+            alphabet.pop(letter)
+    for k in alphabet.keys():
+        return k
+
+
+print(missing_letter("The quick brown box jumps over a lazy dog"))
